b9d0b7701526403b56638826a22ad3173f381563
Laucha Carro <lautaroecarro@gmail.com>
2021-06-13 03:16:44 -0300

    Agregar archivos de proyecto.

---
+++ LatinoNetOnline.Admin/Models/OperationResult.cs
@@ -0,0 +1,23 @@
+﻿namespace LatinoNetOnline.Admin.Models
+{
+    public class OperationResult
+    {
+        public bool IsSuccess { get; set; }
+        public ErrorResult Error { get; set; }
+    }
+
+    public class OperationResult<T> : OperationResult
+    {
+        public T Result { get; set; }
+    }
+
+    public class ErrorResult
+    {
+        public ErrorResult(string code)
+        {
+            Code = code;
+        }
+
+        public string Code { get; set; }
+    }
+}
---
+++ LatinoNetOnline.Admin/Services/ApiClient.cs
@@ -0,0 +1,61 @@
+﻿using LatinoNetOnline.Admin.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace LatinoNetOnline.Admin.Services
+{
+    public interface IApiClient
+    {
+        Task<TResponse> GetAsync<TResponse>(string requestUri) where TResponse : OperationResult;
+        Task<TResponse> PostAsync<TRequest, TResponse>(string requestUri, TRequest requestBody) where TResponse : OperationResult;
+        Task<TResponse> PutAsync<TRequest, TResponse>(string requestUri, TRequest requestBody) where TResponse : OperationResult;
+        Task<TResponse> DeleteAsync<TResponse>(string requestUri) where TResponse : OperationResult;
+        Task<TResponse> DeleteAsync<TRequest, TResponse>(string requestUri, TRequest requestBody) where TResponse : OperationResult;
+    }
+
+    public class ApiClient : IApiClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public ApiClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<TResponse> GetAsync<TResponse>(string requestUri) where TResponse : OperationResult
+        {
+            var httpResponse = await _httpClient.GetAsync(requestUri);
+            return await httpResponse.Content.ReadFromJsonAsync<TResponse>();
+        }
+
+        public async Task<TResponse> PostAsync<TRequest, TResponse>(string requestUri, TRequest requestBody) where TResponse : OperationResult
+        {
+            var httpResponse = await _httpClient.PostAsJsonAsync(requestUri, requestBody);
+
+            return await httpResponse.Content.ReadFromJsonAsync<TResponse>();
+        }
+
+        public async Task<TResponse> PutAsync<TRequest, TResponse>(string requestUri, TRequest requestBody) where TResponse : OperationResult
+        {
+            var httpResponse = await _httpClient.PutAsJsonAsync(requestUri, requestBody);
+
+            return await httpResponse.Content.ReadFromJsonAsync<TResponse>();
+        }
+
+        public async Task<TResponse> DeleteAsync<TResponse>(string requestUri) where TResponse : OperationResult
+        {
+            var httpResponse = await _httpClient.DeleteAsync(requestUri);
+            return await httpResponse.Content.ReadFromJsonAsync<TResponse>();
+        }
+
+        public async Task<TResponse> DeleteAsync<TRequest, TResponse>(string requestUri, TRequest requestBody) where TResponse : OperationResult
+        {
+            return await PostAsync<TRequest, TResponse>(requestUri, requestBody);
+        }
+    }
+}
