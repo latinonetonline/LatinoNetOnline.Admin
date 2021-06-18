95a3bc31ca5b93f799a6758c8f624ffa1f684efe
Laucha Carro <lautaroecarro@gmail.com>
2021-06-18 19:58:40 -0300

    Se implementaron Notificaciones Push

--- LatinoNetOnline.Admin/wwwroot/js/main.js
+++ LatinoNetOnline.Admin/wwwroot/js/main.js
@@ -52,3 +52,112 @@ if ('serviceWorker' in navigator) {
         refreshing = true;
     });
 }
+
+
+//NotificationPush
+const applicationServerPublicKey = 'BLDM-vPYvpbD0r-jj40GOFHjJRRhM48t7UxCpdrfA4g4vrpUtAflP47k3qdBkE-HUTaSalntpGdZzuvrfyx-lgg';
+
+let isSubscribed = false;
+
+function initialiseUI() {
+    pushButton.addEventListener('click', function () {
+        pushButton.disabled = true;
+        if (isSubscribed) {
+            unsubscribeUser();
+        } else {
+            subscribeUser();
+        }
+    });
+
+    // Set the initial subscription value
+    newWorker.pushManager.getSubscription()
+        .then(function (subscription) {
+            isSubscribed = !(subscription === null);
+
+            updateSubscriptionOnServer(subscription);
+
+            if (isSubscribed) {
+                console.log('User IS subscribed.');
+            } else {
+                console.log('User is NOT subscribed.');
+            }
+
+            updateBtn();
+        });
+}
+
+
+
+function subscribeUser() {
+    const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+    newWorker.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: applicationServerKey
+    })
+        .then(function (subscription) {
+            console.log('User is subscribed:', subscription);
+
+            updateSubscriptionOnServer(subscription);
+
+            isSubscribed = true;
+
+            updateBtn();
+        })
+        .catch(function (err) {
+            console.log('Failed to subscribe the user: ', err);
+            updateBtn();
+        });
+}
+
+
+function updateSubscriptionOnServer(subscription) {
+    // TODO: Send subscription to application server
+
+    const subscriptionJson = document.querySelector('.js-subscription-json');
+    const subscriptionDetails =
+        document.querySelector('.js-subscription-details');
+
+    if (subscription) {
+        console.log(subscription);
+        subscriptionJson.textContent = JSON.stringify(subscription);
+        subscriptionDetails.classList.remove('is-invisible');
+    } else {
+        subscriptionDetails.classList.add('is-invisible');
+    }
+}
+
+
+function unsubscribeUser() {
+    newWorker.pushManager.getSubscription()
+        .then(function (subscription) {
+            if (subscription) {
+                return subscription.unsubscribe();
+            }
+        })
+        .catch(function (error) {
+            console.log('Error unsubscribing', error);
+        })
+        .then(function () {
+            updateSubscriptionOnServer(null);
+
+            console.log('User is unsubscribed.');
+            isSubscribed = false;
+
+            updateBtn();
+        });
+}
+
+function urlB64ToUint8Array(base64String) {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
--- LatinoNetOnline.Admin/wwwroot/service-worker.js
+++ LatinoNetOnline.Admin/wwwroot/service-worker.js
@@ -2,3 +2,30 @@
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
 self.addEventListener('fetch', () => { });
+
+
+self.addEventListener('push', function (event) {
+    console.log('[Service Worker] Push Received.');
+    console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+    const payload = event.data.json();
+
+    const title = 'Latino .NET Online';
+    const options = {
+        body: payload.message,
+        icon: 'icon-512.png',
+        badge: 'icon-512.png',
+        data: { url: payload.url }
+    };
+
+    const notificationPromise = self.registration.showNotification(title, options);
+    event.waitUntil(notificationPromise);
+});
+
+self.addEventListener('notificationclick', function (event) {
+    console.log('[Service Worker] Notification click Received.');
+
+    event.notification.close();
+
+    event.waitUntil(clients.openWindow(event.notification.data.url));
+});
